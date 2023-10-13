adcf4dc25df81a75444797a82e3689cfedcd2e81
Maker-Hub.de <147385477+Maker-Hub-De@users.noreply.github.com>
2023-10-13 21:29:37 +0200

    Update hetzerDnsUpdate.php

--- hetzerDnsUpdate.php
+++ hetzerDnsUpdate.php
@@ -34,9 +34,6 @@ Hetzner DNS Zone update<br>
         <input type="text" class="form-control formadd" name="apiToken" id="apiToken" maxlength="32" value="<?php if (isset($objConfig) && property_exists($objConfig, 'apiToken')) echo $objConfig->apiToken; ?>">
         <span class="help-block">Enter Hetzer DNS API access tokens</span>
     </div>
-  </div>
-
-  <div class="form-group">
     <label class="col-lg-2 col-md-3 control-label" for="">API-Token:</label>
       <div class="col-lg-10 col-md-9">
         <input type="text" class="form-control formadd" name="directory" id="directory" maxlength="255" value="<?php if (isset($objConfig) && property_exists($objConfig, 'apiToken')) echo $objConfig->apiToken; ?>">
