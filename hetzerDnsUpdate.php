f474ae89d41de5b5e491b83879b28ec7f685aade
Maker-Hub.de <147385477+Maker-Hub-De@users.noreply.github.com>
2023-10-13 21:31:04 +0200

    Update hetzerDnsUpdate.php

--- hetzerDnsUpdate.php
+++ hetzerDnsUpdate.php
@@ -36,7 +36,7 @@ Hetzner DNS Zone update<br>
     </div>
     <label class="col-lg-2 col-md-3 control-label" for="">API-Token:</label>
       <div class="col-lg-10 col-md-9">
-        <input type="text" class="form-control formadd" name="directory" id="directory" maxlength="255" value="<?php if (isset($objConfig) && property_exists($objConfig, 'apiToken')) echo $objConfig->apiToken; ?>">
+        <input type="text" class="form-control formadd" name="directory" id="directory" maxlength="255"         <input type="text" class="form-control formadd" name="directory" id="directory" maxlength="255" value="<?php if (isset($objConfig) && property_exists($objConfig, 'directory')) echo $objConfig->directory; else echo '/var/named' ?>">
         <span class="help-block">The directory that should be monitored for changes (default is '/var/named').</span>
     </div>
   </div>    
