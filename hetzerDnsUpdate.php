5b3999f30fddcc477ed88ca7647f40c39aa6369c
Maker-Hub.de <147385477+Maker-Hub-De@users.noreply.github.com>
2023-10-13 21:27:47 +0200

    Update hetzerDnsUpdate.php

--- hetzerDnsUpdate.php
+++ hetzerDnsUpdate.php
@@ -17,18 +17,17 @@ if ($objFileHandler !== false) {
 
 Hetzner DNS Zone update<br>
 <form class="form-horizontal group-border stripped" action="" method="post">
-
   <div class="form-group">
     <label class="col-lg-2 col-md-3 control-label" for="">Aktive:</label>
       <div class="col-lg-10 col-md-9">
-    <div class="toggle-custom toggle-inline">
-      <label class="toggle tip" data-original-title="Status" data-on="ON" data-off="OFF">
-        <input type="checkbox" class="" id="checkbox-toggle" checked="<?php if (isset($objConfig) && property_exists($objConfig, 'apiToken') && $objConfig->active == true) echo 'checked'; ?>" name="checkbox-toggle">
-        <span class="button-checkbox"></span>
-      </label>
-    </div>    </div>
+        <div class="toggle-custom toggle-inline">
+          <label class="toggle tip" data-original-title="Status" data-on="ON" data-off="OFF">
+            <input type="checkbox" class="" id="checkbox-toggle" checked="<?php if (isset($objConfig) && property_exists($objConfig, 'apiToken') && $objConfig->active == true) echo 'checked'; ?>" name="checkbox-toggle">
+            <span class="button-checkbox"></span>
+          </label>
+        </div>
+      </div>
   </div>
-
   <div class="form-group">
     <label class="col-lg-2 col-md-3 control-label" for="">API-Token:</label>
       <div class="col-lg-10 col-md-9">
@@ -36,10 +35,12 @@ Hetzner DNS Zone update<br>
         <span class="help-block">Enter Hetzer DNS API access tokens</span>
     </div>
   </div>
+
   <div class="form-group">
-    <label class="col-lg-2 col-md-3 control-label" for="">Directory:</label>
-      <input type="text" class="form-control formadd" name="directory" id="directory" maxlength="255" value="<?php if (isset($objConfig) && property_exists($objConfig, 'directory')) echo $objConfig->directory; else echo '/var/named' ?>">
+    <label class="col-lg-2 col-md-3 control-label" for="">API-Token:</label>
+      <div class="col-lg-10 col-md-9">
+        <input type="text" class="form-control formadd" name="directory" id="directory" maxlength="255" value="<?php if (isset($objConfig) && property_exists($objConfig, 'apiToken')) echo $objConfig->apiToken; ?>">
         <span class="help-block">The directory that should be monitored for changes (default is '/var/named').</span>
     </div>
-  </div>
+  </div>    
 </form>
