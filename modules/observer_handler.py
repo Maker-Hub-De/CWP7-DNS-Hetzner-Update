02b9da1206b631060b8abc8eed145cb1c05f7027
Maker-Hub.de <147385477+Maker-Hub-De@users.noreply.github.com>
2023-10-15 21:06:57 +0200

    Update observer_handler.py

--- modules/observer_handler.py
+++ modules/observer_handler.py
@@ -50,7 +50,7 @@ class ObserverHandler(FileSystemEventHandler):
         for file_name in file_list:
             # Use an absolute path to the file
             file_path = os.path.abspath(os.path.join(self.directory, file_name))
-            
+
             if not file_name.endswith('.db') \
             or not os.path.isfile(file_path) \
             or file_name == "hydrogen.ns.hetzner.com.db" \
@@ -58,7 +58,7 @@ class ObserverHandler(FileSystemEventHandler):
             or file_name == "helium.ns.hetzner.com.db":
                 # File/directory is not relevant; Dosen't need a log entry
                 continue
-            
+
             if not os.path.exists(file_path):
                 # That should never happen, because we read the directory directly before but who knows :-)
                 self.logger.error(f"File {file_path} not found.")
@@ -72,13 +72,13 @@ class ObserverHandler(FileSystemEventHandler):
                 # The file was never checked => it could be a new zone or a file that was never checked before
                 # Getting domain from file name
                 domain = hetzner_dns.get_domain(file_name)
-                
+
                 # Try to get a zone ID; the zone may already exist and only the database entry was missing
                 zone_id = hetzner_dns.get_zone_id(domain)
-                
+
                 if zone_id == None:  # We don't have an existing zone
                     domain = hetzner_dns.create_zone(domain)
-                    
+
                 if zone_id == None: # Now we should have a zone id; if not, there is a problem in the DNS app.
                     self.logger.error("Could not create a new zone")
                     continue  # Continue to the next file
@@ -88,12 +88,12 @@ class ObserverHandler(FileSystemEventHandler):
                     # The log will be written within the method update_zone_from_file
                     # we just need to contiue to not update the database and can try it the next time
                     continue  # Continue to the next file
-                
+
                 # Adding new file to the database
                 if not self.db_manager.insert_file_info(file_name, last_modified_file, current_check_time)
                     self.logger.error(f"Could not insert file {file_name} in database.")
                     continue # Continue to the next file
-                
+
             # Checking if the file was modified
             elif last_modified_db == last_modified_file:
                 # No modification found => just update the check time
@@ -110,10 +110,10 @@ class ObserverHandler(FileSystemEventHandler):
             and  last_checked < current_check_time - 2:        
                 # Found a changed file that is relevant
                 domain = hetzner_dns.get_domain(file_name)
-                
+
                 # Try to get a zone ID; the zone may already exist
                 zone_id = hetzner_dns.get_zone_id(domain)
-                
+
                 if zone_id == None: # Now we schould have a zone id; if not go on
                     domain = hetzner_dns.create_zone(domain)
 
@@ -127,7 +127,7 @@ class ObserverHandler(FileSystemEventHandler):
                     # The log will be written within the method update_zone_from_file
                     # we just need to contiue to not update the database and can try it the next time
                     continue  # Continue to the next file
-                    
+
                 # Updating the database
                 if not self.db_manager.update_file_info(file_name, last_modified_file, current_check_time):
                     self.logger.error(f"Could not update file {file_name} in database.")
@@ -152,15 +152,15 @@ class ObserverHandler(FileSystemEventHandler):
         for file_name in files_in_db:
             # Use an absolute path to the file
             file_path = os.path.abspath(os.path.join(self.directory, file_name[0]))
-                           
+
             # Check if the file still exists on the file system
             if not os.path.exists(file_path): # File was deleted
                 # Getting domain from filename
                 domain = hetzner_dns.get_domain(file_name[0])
-                
+
                 # Seaching the zone id
                 zone_id = hetzner_dns.get_zone_id(domain)
-                
+
                 if zone_id: # We found a zone id
                      # Deleting the zone id
                     if not hetzner_dns.delete_zone(domain):
